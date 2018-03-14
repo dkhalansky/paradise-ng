5ac024d7838790e8416ea58168817d74cfc50e7d
Dmitry Khalansky <vault123dweller@gmail.com>
2018-03-14 13:37:05 +0300

    Implement traversing the tree
    The first, crude approach to finding the subtrees of a given AST
    that are decorated: traverse through all the trees and store a list
    of the ones that match.

--- src/main/scala/plugin/ParadiseNg.scala
+++ src/main/scala/plugin/ParadiseNg.scala
@@ -6,6 +6,7 @@ import nsc.Global
 import nsc.Phase
 import nsc.plugins.Plugin
 import nsc.plugins.PluginComponent
+import scala.collection.mutable.ListBuffer
 
 class ParadiseNg(val global: Global) extends Plugin {
     import global._
@@ -14,7 +15,8 @@ class ParadiseNg(val global: Global) extends Plugin {
     val components = new ParadiseNgComponent(this, global) :: Nil
 }
 
-class ParadiseNgComponent(plugin: Plugin, val global: Global) extends PluginComponent {
+class ParadiseNgComponent(plugin: Plugin, val global: Global)
+extends PluginComponent {
     import global._
 
     val phaseName = "paradise-ng"
@@ -24,6 +26,43 @@ class ParadiseNgComponent(plugin: Plugin, val global: Global) extends PluginComp
 
     private class ParadiseNgPhase(prev: Phase) extends StdPhase(prev) {
         def apply(unit: CompilationUnit) : Unit = {
+            val isMyAnnot = (v : AnnotationInfo) =>
+                v.atp <:< typeOf[ParadiseNgAnnotation]
+            val treesWithAnnotations = findAnnotated(isMyAnnot, unit.body)
+            for ((t, an) <- treesWithAnnotations) {
+                println(t)
+                for (a <- an) {
+                    println(a)
+                }
+                println("-------------")
+            }
         }
+    }
+
+    def findAnnotated(predicate : AnnotationInfo => Boolean, tree : Tree) :
+    List[(Tree, List[AnnotationInfo])] = {
+        def annotationsOf(tree: Tree) : List[AnnotationInfo] = tree match {
+            case t: MemberDef => t.symbol.annotations
+            case Typed(_, tt) if tt.tpe != null => tt.tpe.annotations
+            case _ => Nil
+        }
+
+        var trees = new ListBuffer[(Tree, List[AnnotationInfo])]()
+
+        object traverser extends Traverser {
+            override def traverse(tree: Tree): Unit = {
+                annotationsOf(tree).filter(predicate) match {
+                    case Nil => super.traverse(tree)
+                    case lst => {
+                        trees += new Tuple2(tree, lst)
+                        super.traverse(tree)
+                    }
+                }
+            }
+        }
+
+        traverser.traverse(tree)
+
+        trees.toList
     }
 }
