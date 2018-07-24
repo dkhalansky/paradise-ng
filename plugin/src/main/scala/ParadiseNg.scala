f4b270996a6f3a3e290ae0e37ee9731880866af4
Dmitry Khalansky <vault123dweller@gmail.com>
2018-07-24 11:54:03 +0300

    Deal with companion objects in code blocks
    Now companion objects are found even if they are defined in code
    blocks which, for some unknown reason, are problematic.

--- plugin/src/main/scala/ParadiseNg.scala
+++ plugin/src/main/scala/ParadiseNg.scala
@@ -36,12 +36,8 @@ extends PluginComponent {
                     new ScalametaTransformer(metatree)
                 }
                 for ((md, ans) <- ourAnnottees(typed).reverse) {
-                    val companionPos = {
-                        val sym = md.symbol.companion
-                        if (sym == null || sym == NoSymbol) {
-                            None
-                        } else getSource(sym).map(t => t.pos.start-1)
-                    }
+                    val companionPos = getCompanionTree(md)
+                        .map(t => t.pos.start-1)
                     val start : scala.meta.Stat => scala.meta.Stat = m => m
                     var fn = (start /: ans) {
                         (f, an) => getAnnotationFunction(an) compose f
@@ -55,6 +51,80 @@ extends PluginComponent {
             }
         }
         phase
+    }
+
+    /* Get the tree corresponding to the companion object of the member,
+       if any. */
+    def getCompanionTree(original: MemberDef) : Option[ModuleDef] = {
+        // If the compiler itself can point us to a companion, we trust it.
+        getSource(original.symbol.companion) match {
+            case Some(m) => return Some(m.asInstanceOf[ModuleDef])
+            case _ =>
+        }
+
+        /* If the compiler didn't find the companion object, it can mean one
+           of two things: either there really is no companion object or
+           the compiler is mistaken.
+
+           According to the definition of and comments for
+           `def companionSymbolOf` from `Namers.scala` in scalac sources, the
+           compiler can be trusted iff the member alongside its companion
+           object is *not* defined in a code block. Class and package
+           definitions are not considered code blocks, they are called
+           "templates". So, for
+
+               {
+                   class A
+                   object A
+               }
+
+           and
+
+               def foo() {
+                   class A
+                   object A
+               }
+
+           lookup fails, while it succeeds for, say,
+
+               class B {
+                   class A
+                   object A
+               }
+
+           So, if the compiler has told us that it knows nothing about a
+           companion object, we trust it if the member was not defined in a
+           code block. */
+        val owner = original.symbol.owner
+        if (!owner.isTerm && owner.hasCompleteInfo) {
+            return None
+        }
+
+        /* We take the source code of the parent and try and traverse its tree
+           looking for something that looks like our companion object to us. */
+
+        val parent = getSource(owner) match {
+            case Some(p) => p
+            case _ => return None
+        }
+
+        var result = None.asInstanceOf[Option[ModuleDef]]
+
+        object traverser extends Traverser {
+            override def traverse(tree: Tree) {
+                tree match {
+                    case m : ModuleDef if m.symbol != null &&
+                        m.symbol.name == original.symbol.name.companionName &&
+                        m.symbol.isCoDefinedWith(original.symbol) => {
+                            result = Some(m)
+                        }
+                    case _ => super.traverse(tree)
+                }
+            }
+        }
+        traverser.traverse(parent)
+
+        result
     }
 
     /* Given an AnnotationInfo (received by typechecking a tree and looking at
