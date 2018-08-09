ff95f350f688a47762de1400485e350d0a599659
dkhalansky <vault123dweller@gmail.com>
2018-08-09 18:00:19 +0300

    Simplify class instantiation code
    Now, we just try every constructor in order, and if one of them
    returns a proper value, we return it.
    
    This has arisen because now we also need to pass as parameters
    objects more complex than the primitive values. The code shouldn't
    cause problems except in some obscure cases when one set of
    parameters can be used for many constructors, and in such cases,
    we can't even tell the constructors apart anyway.
    
    Also, it seems that multiple constructors are used rarely in Scala.
    We could even refuse to work altogether if more than one
    constructor was defined and probably no one would complain, but the
    current approach didn't require more effort and covered more cases.

--- plugin/src/main/scala/Reflect.scala
+++ plugin/src/main/scala/Reflect.scala
@@ -1,7 +1,6 @@
 package localhost.plugin
 import scala.reflect.io.AbstractFile
 import java.net.URL
-import scala.reflect.api.Symbols
 
 /* A set of functions to deal with all the reflective needs. */
 object Reflect {
@@ -35,55 +34,19 @@ object Reflect {
         }
     }
 
-    /* This is one strange function! What does it do?
-       Well, in Scala, basic types (such as Int or Byte) can map to boxed
-       values, that is, be stored on the heap, or they can be stored on
-       the stack.
-
-       In Java, these were separate types: `Integer` denoted the boxed
-       variant and `int`-- the unboxed one. In Scala, it's all completely
-       transparent.
-
-       It is a good thing, but here comes a problem. Let's say we needed to
-       use a constructor that accepted an `Int`. A user gives us its
-       argument, `3`. If we treat this `3` as a value and ask what its class
-       is, Scala answers, `java.lang.Integer`. But the constuctor was
-       specialized to accept an unboxed `int`!
-
-       So, we need to find the constructor with the given signature, and to
-       infer the signature from the arguments that the user provided. And from
-       the reflection's point of view, it matters whether a function accepts
-       boxed or unboxed values: there can be two different functions,
-       `f(Integer i)` and `f(int i)`, in the same class.
-
-       Thus, this function. It assumes that the user specified their annotation
-       with an `Int` or a `Boolean` and not with `java.lang.Integer` and tries
-       to find the unboxed class descriptions where possible.
-    */
-    private def getWrappedClass(v: Any): Class[_] = v match {
-        case x: Unit    => x.getClass
-        case x: Boolean => x.getClass
-        case x: Byte    => x.getClass
-        case x: Short   => x.getClass
-        case x: Int     => x.getClass
-        case x: Long    => x.getClass
-        case x: Float   => x.getClass
-        case x: Double  => x.getClass
-        case x: Char    => x.getClass
-        case _          => v.getClass
-    }
-
     /* Instantiate a class by the given name with the given arguments to
        the constructor. */
     def instantiate[T](
         loader: ClassLoader, className: String, args: Seq[Any]
     ): T = {
-        val arg_types = args.map(xs => getWrappedClass(xs))
         val cls = Class.forName(className, true, loader)
         val nargs = args.map(t => t.asInstanceOf[AnyRef]).toArray
-        cls.getConstructor(arg_types: _*)
-            .newInstance(nargs: _*)
-            .asInstanceOf[T]
+        for (ctr <- cls.getConstructors) {
+            try return ctr.newInstance(nargs: _*).asInstanceOf[T] catch {
+                case e: java.lang.IllegalArgumentException =>
+            }
+        }
+        throw new java.lang.NoSuchMethodException
     }
 
 }
