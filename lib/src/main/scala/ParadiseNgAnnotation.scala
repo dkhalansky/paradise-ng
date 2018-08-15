dd0d51277f7ebe19fc3a3b4bdc65e21690058f3c
dkhalansky <vault123dweller@gmail.com>
2018-08-15 16:51:28 +0300

    Move the modifier-handling logic to a separate file

--- lib/src/main/scala/ParadiseNgAnnotation.scala
+++ lib/src/main/scala/ParadiseNgAnnotation.scala
@@ -31,32 +31,10 @@ trait ParadiseNgAnnotation extends StaticAnnotation with TreeTransformation {
     def apply(annottee: Stat): Stat
 }
 
-object Modifiers {
-    implicit class XtensionDefModifiers[T <: Defn](val tree: T) extends AnyVal {
-        def transformMods(fn: List[Mod] => List[Mod]): T = tree match {
-            case t: Defn.Class  => t.copy(mods = fn(t.mods)).asInstanceOf[T]
-            case t: Defn.Trait  => t.copy(mods = fn(t.mods)).asInstanceOf[T]
-            case t: Defn.Type   => t.copy(mods = fn(t.mods)).asInstanceOf[T]
-            case t: Defn.Object => t.copy(mods = fn(t.mods)).asInstanceOf[T]
-            case t: Defn.Def    => t.copy(mods = fn(t.mods)).asInstanceOf[T]
-            case t: Defn.Val    => t.copy(mods = fn(t.mods)).asInstanceOf[T]
-            case t: Defn.Var    => t.copy(mods = fn(t.mods)).asInstanceOf[T]
-        }
-    }
-
-    implicit class XtensionDeclModifiers[T <: Decl](val tree: T) extends AnyVal {
-        def transformMods(fn: List[Mod] => List[Mod]): T = tree match {
-            case t: Decl.Type   => t.copy(mods = fn(t.mods)).asInstanceOf[T]
-            case t: Decl.Def    => t.copy(mods = fn(t.mods)).asInstanceOf[T]
-            case t: Decl.Val    => t.copy(mods = fn(t.mods)).asInstanceOf[T]
-        }
-    }
-}
-
 class ParadiseNgAnnotationCombination(ans: List[(ParadiseNgAnnotation, Int)])
 extends TreeTransformation {
     override def pluginInterop(tree: Stat, comp: Option[Stat]) = {
-        import Modifiers._
+        import localhost.lib.internal.meta.Modifiers._
         val badIndices = ans map { m => m._2 }
         val removeAnnots = (mods: List[Mod]) =>
             mods.zipWithIndex
---
+++ lib/src/main/scala/internal/meta/Modifiers.scala
@@ -0,0 +1,46 @@
+package localhost.lib.internal.meta
+import scala.meta._
+
+object Modifiers {
+    implicit class XtensionDefModifiers[T <: Defn](val tree: T) extends AnyVal {
+        def transformMods(fn: List[Mod] => List[Mod]): T =
+            tree.withMods(fn(tree.mods))
+
+        def mods: List[Mod] = tree match {
+            case t: Defn.Class  => t.mods
+            case t: Defn.Trait  => t.mods
+            case t: Defn.Type   => t.mods
+            case t: Defn.Object => t.mods
+            case t: Defn.Def    => t.mods
+            case t: Defn.Val    => t.mods
+            case t: Defn.Var    => t.mods
+        }
+
+        def withMods(mods: List[Mod]) = tree match {
+            case t: Defn.Class  => t.copy(mods = mods).asInstanceOf[T]
+            case t: Defn.Trait  => t.copy(mods = mods).asInstanceOf[T]
+            case t: Defn.Type   => t.copy(mods = mods).asInstanceOf[T]
+            case t: Defn.Object => t.copy(mods = mods).asInstanceOf[T]
+            case t: Defn.Def    => t.copy(mods = mods).asInstanceOf[T]
+            case t: Defn.Val    => t.copy(mods = mods).asInstanceOf[T]
+            case t: Defn.Var    => t.copy(mods = mods).asInstanceOf[T]
+        }
+    }
+
+    implicit class XtensionDeclModifiers[T <: Decl](val tree: T) extends AnyVal {
+        def transformMods(fn: List[Mod] => List[Mod]): T =
+            tree.withMods(fn(tree.mods))
+
+        def mods: List[Mod] = tree match {
+            case t: Decl.Type   => t.mods
+            case t: Decl.Def    => t.mods
+            case t: Decl.Val    => t.mods
+        }
+
+        def withMods(mods: List[Mod]) = tree match {
+            case t: Decl.Type   => t.copy(mods = mods).asInstanceOf[T]
+            case t: Decl.Def    => t.copy(mods = mods).asInstanceOf[T]
+            case t: Decl.Val    => t.copy(mods = mods).asInstanceOf[T]
+        }
+    }
+}
