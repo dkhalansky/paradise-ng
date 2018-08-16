764c8efa22e1995e17e8be97f0f179adb68903b2
dkhalansky <vault123dweller@gmail.com>
2018-08-17 02:15:27 +0300

    Drastically simplify the logic behind transformations
    Before, there was an extremely complicated scheme of shadow trees
    that involved storing changes either in the parent or in the tree
    itself. Now, everything is much simpler since only each tree stores
    its own expansions. Now, the shadow world isn't affected by the
    strange mechanics of comparing trees.

--- plugin/src/main/scala/meta/ParadiseNgTransformer.scala
+++ plugin/src/main/scala/meta/ParadiseNgTransformer.scala
@@ -7,64 +7,54 @@ class ParadiseNgTransformer(var tree: Tree) {
     import TreeStorage._
     import Transversers._
 
-    private def contains(p: Position, i: Int) : Boolean = {
-        p.start <= i && p.end > i
-    }
+    private type Payload = (List[Stat], List[Stat])
 
-    private def getAt(position: Int) : Stat = {
-        val f = (u: Stat) => return u;
-        (new Traverser {
-            override def apply(tree: Tree) {
-                if (contains(tree.getPosition(), position)) {
-                    super.apply(tree.getPayload[Tree]().getOrElse(tree))
-                    tree match {
-                        case u: Stat => f(u)
-                        case _ =>
-                    }
-                }
-            }
-        })(tree)
-        None.get
-    }
-
-    private def replaceChild(parent: Tree, child: Stat, newchildren: List[Stat])
-    = {
-        def newStats(old: List[Stat]) : List[Stat] = old.flatMap {
-            case v if v.equals(child) => newchildren
-            case v => List(v)
+    private def newStats(stats: List[Stat]): List[Stat] = stats flatMap { s =>
+        s.getPayload[Payload] match {
+            case None => List(s)
+            case Some((mn, arb)) => mn ++ arb
         }
-        val newParent = parent.getPayload[Tree]().getOrElse(parent) match {
-            case o @ Template(a, b, c, stats) => o.copy(stats = newStats(stats))
-            case o @ Source(stats)            => o.copy(stats = newStats(stats))
-            case o @ Term.Block(stats)        => o.copy(stats = newStats(stats))
-            case o @ Pkg(a, stats)            => o.copy(stats = newStats(stats))
-        }
-        parent storePayload newParent
     }
 
-    private def eval(tree: Tree) = tree.transform {
-        case s => s.getPayload[Tree]().getOrElse(s)
+    private def eval(tree: Tree): Tree = tree.transform {
+        case o @ Template(a, b, c, stats) => o.copy(stats = newStats(stats))
+        case o @ Source(stats)            => o.copy(stats = newStats(stats))
+        case o @ Term.Block(stats)        => o.copy(stats = newStats(stats))
+        case o @ Pkg(a, stats)            => o.copy(stats = newStats(stats))
+    }
+
+    private def getAt(position: Int, t: Tree = tree): Stat = {
+        t.findPos[Stat](Position.Range(null, position, position+1),
+            _.getPosition()).get
     }
 
     def modify(position: Int, companionPos: Option[Int], fn: TreeTransformation)
     {
-        val nonShadowParent = {
-            val nonShadowTree = this.tree.findPos[Stat](
-                Position.Range(null, position, position+1),
-                t => t.getPosition).get
-            nonShadowTree.parent.get
-        }
         val tree = getAt(position)
-        val companion = companionPos.map(getAt)
-        val expanded = fn.pluginInterop(eval(tree).asInstanceOf[Stat],
-            companion.map(c => eval(c).asInstanceOf[Stat]))
-        val newTrees = (expanded._1.map(_._1)
-            ++ expanded._1.flatMap(_._2)
-            ++ expanded._2)
-        companion map { c => c storePayload (q"{}") }
-        newTrees match {
-            case List(t) => tree storePayload t
-            case lst => replaceChild(nonShadowParent, tree, lst)
+        companionPos match {
+            case None => {
+                val result = fn.pluginInterop(
+                    eval(tree).asInstanceOf[Stat], None)
+                val newTrees = (result._1.map(_._1) ++ result._1.flatMap(_._2),
+                    result._2)
+                tree storePayload newTrees
+            }
+            case Some(cpos) => {
+                val compStat = getAt(cpos, tree.parent.get)
+                val compPayload = compStat
+                    .getPayload[Payload]
+                    .getOrElse((List(compStat), Nil))
+                val companion = compPayload match {
+                    case (List(c), _) => c
+                }
+                val result = fn.pluginInterop(
+                    eval(tree).asInstanceOf[Stat], Some(companion))
+                val newTrees = (result._1.map(_._1),
+                    result._2)
+                val companions = (result._1.flatMap(_._2), compPayload._2)
+                tree storePayload newTrees
+                compStat storePayload companions
+            }
         }
     }
 
